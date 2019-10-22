e913a9ebd128f6007aa548fa061569fdbd323d04
tringler <mail@thomas-ringler.de>
2019-10-22 21:17:36 +0200

    Update run.sh

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -2,16 +2,16 @@
 
 #Homegear installation - https://homegear.eu/downloads.html
 echo "[INFO] Install Homegear"
-apt update && \
-apt install -y apt-transport-https && \
-curl https://apt.homegear.eu/Release.key | apt-key add - && \
-echo 'deb https://apt.homegear.eu/Ubuntu/ xenial/' > /etc/apt/sources.list.d/homegear.list && \
-\
-apt update && \
+apt update
+apt install -y apt-transport-https 
+curl https://apt.homegear.eu/Release.key | apt-key add - 
+echo 'deb https://apt.homegear.eu/Ubuntu/ xenial/' > /etc/apt/sources.list.d/homegear.list 
+
+apt update 
 apt install -y homegear homegear-management homegear-adminui homegear-nodes-core homegear-nodes-extra homegear-homematicbidcos \
     homegear-homematicwired homegear-insteon homegear-max homegear-philipshue homegear-sonos homegear-kodi \
     homegear-ipcam homegear-beckhoff homegear-knx homegear-enocean homegear-intertechno homegear-nanoleaf \
-    homegear-ccu homegear-mbus homegear-influxdb \
+    homegear-ccu homegear-mbus homegear-influxdb
 
 mkdir -p /config/homegear
 mkdir -p /config/homegear/config
