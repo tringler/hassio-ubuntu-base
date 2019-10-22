e5e6d1fdc06d1293c929eb2b0583db69d9171b5f
tringler <mail@thomas-ringler.de>
2019-10-22 16:42:52 +0200

    .

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p /config/homegear
-mkdir -p /config/homegear/config
-mkdir -p /config/homegear/data
-mkdir -p /config/homegear/log
+#mkdir -p /config/homegear
+#mkdir -p /config/homegear/config
+#mkdir -p /config/homegear/data
+#mkdir -p /config/homegear/log
 
-echo "[INFO] Start Homegear"
-service homegear start
-service homegear-management start
-service homegear-influxdb start
-tail -f /var/log/homegear/homegear.log
+#echo "[INFO] Start Homegear"
+#service homegear start
+#service homegear-management start
+#service homegear-influxdb start
+tail -f /dev/null
