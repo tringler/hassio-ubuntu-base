71e3091aa4c36b7b9bce2320c433dd94865fc343
tringler <mail@thomas-ringler.de>
2019-10-22 15:47:19 +0200

    Update run.sh

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-tail -f /dev/null
+
+mkdir -p /config/homegear
+mkdir -p /config/homegear/config
+mkdir -p /config/homegear/data
+mkdir -p /config/homegear/log
+
+echo "[INFO] Start Homegear"
+service homegear start
+service homegear-management start
+service homegear-influxdb start
+tail -f /var/log/homegear/homegear.log && 
+child=$!
+wait "$child"
