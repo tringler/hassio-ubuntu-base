fb88d92559c6e3c56914929322eb0207ad9a6d15
tringler <mail@thomas-ringler.de>
2019-10-22 21:46:13 +0200

    Update run.sh

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -17,4 +17,4 @@ echo "[INFO] Start Homegear"
 service homegear start
 service homegear-management start
 service homegear-influxdb start
-tail -f /var/log/homegear.log
+tail -f /var/log/homegear/homegear.log
