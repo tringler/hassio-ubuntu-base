82607a1ae01c197a4e40b9be36da642aad99f58a
tringler <mail@thomas-ringler.de>
2019-10-23 21:15:14 +0200

    .

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#Homegear installation - https://homegear.eu/downloads.html
-echo "[INFO] Install Homegear"
-apt update 
-apt install -y apt-transport-https 
-curl https://apt.homegear.eu/Release.key | apt-key add - 
-echo 'deb https://apt.homegear.eu/Ubuntu/ xenial/' > /etc/apt/sources.list.d/homegear.list 
-
-apt update 
-apt install -y homegear homegear-management homegear-adminui homegear-nodes-core homegear-nodes-extra homegear-homematicbidcos \
-    homegear-homematicwired homegear-insteon homegear-max homegear-philipshue homegear-sonos homegear-kodi \
-    homegear-ipcam homegear-beckhoff homegear-knx homegear-enocean homegear-intertechno homegear-nanoleaf \
-    homegear-ccu homegear-mbus homegear-influxdb
-
-rm /var/lib/homegear/homegear_updated
-
 echo "[INFO] Start Homegear"
 service homegear start
 service homegear-management start
