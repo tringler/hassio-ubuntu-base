16776cb31924a82324ca957acbd32f24b0db7d13
tringler <mail@thomas-ringler.de>
2019-10-24 23:22:34 +0200

    .

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -15,7 +15,6 @@ touch /config/homegear/log/homegear.log
 
 mkdir -p /var/run/homegear
 chown homegear:homegear /var/run/homegear
-chown -R homegear:homegear /config/homegear/config
 /etc/homegear/homegear-start.sh
 homegear -c /config/homegear/config -u homegear -g homegear -p /var/run/homegear/homegear.pid &
 homegear-management -p /var/run/homegear/homegear-management.pid &
