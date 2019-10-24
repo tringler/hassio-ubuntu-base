e4fc184f0c89c8f15a4dabcadc689a6a069089aa
tringler <mail@thomas-ringler.de>
2019-10-24 13:52:08 +0200

    .

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+_term() {
+    kill $(cat /var/run/homegear/homegear-management.pid)
+    kill $(cat /var/run/homegear/homegear-webssh.pid)
+    kill $(cat /var/run/homegear/homegear.pid)
+    /etc/homegear/homegear-stop.sh
+    exit $?
+}
 
-echo "[INFO] Start Homegear"
-service homegear start
-service homegear-management start
-service homegear-influxdb start
-tail -f /config/homegear/log/homegear.log
+trap _term SIGTERM
+
+mkdir -p /var/run/homegear
+chown homegear:homegear /var/run/homegear
+/etc/homegear/homegear-start.sh
+homegear -c /config/homegear/config -u homegear -g homegear -p /var/run/homegear/homegear.pid &
+homegear-management -p /var/run/homegear/homegear-management.pid &
+homegear-webssh -p /var/run/homegear/homegear-webssh.pid &
+tail -f /var/log/homegear/homegear.log &
+child=$!
+wait "$child"
