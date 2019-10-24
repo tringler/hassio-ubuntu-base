dbb0ad15fe8b9f750fe1fe4e78006f1445f7e07f
tringler <mail@thomas-ringler.de>
2019-10-24 22:51:03 +0200

    Update run.sh

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-tail -f /dev/null
-#_term() {
-#    kill $(cat /var/run/homegear/homegear-management.pid)
-#    kill $(cat /var/run/homegear/homegear-webssh.pid)
-#    kill $(cat /var/run/homegear/homegear.pid)
-#    /etc/homegear/homegear-stop.sh
-#    exit $?
-#}
+_term() {
+    kill $(cat /var/run/homegear/homegear-management.pid)
+    kill $(cat /var/run/homegear/homegear-webssh.pid)
+    kill $(cat /var/run/homegear/homegear.pid)
+    /etc/homegear/homegear-stop.sh
+    exit $?
+}
 
-#trap _term SIGTERM
+trap _term SIGTERM
 
-#cp -R /etc/homegear/* /config/homegear/config
-#mv /main.conf /config/homegear/config/main.conf
+cp -R /etc/homegear/* /config/homegear/config
+mv /main.conf /config/homegear/config/main.conf
+touch /config/homegear/log/homegear.log
 
-#mkdir -p /var/run/homegear
-#chown homegear:homegear /var/run/homegear
-#/etc/homegear/homegear-start.sh
-#homegear -c /config/homegear/config -u homegear -g homegear -p /var/run/homegear/homegear.pid &
-#homegear-management -p /var/run/homegear/homegear-management.pid &
-#tail -f /var/log/homegear/homegear.log &
-#child=$!
-#wait "$child"
+mkdir -p /var/run/homegear
+chown homegear:homegear /var/run/homegear
+/etc/homegear/homegear-start.sh
+homegear -c /config/homegear/config -u homegear -g homegear -p /var/run/homegear/homegear.pid &
+homegear-management -p /var/run/homegear/homegear-management.pid &
+tail -f /config/homegear/log/homegear.log &
+child=$!
+wait "$child"
