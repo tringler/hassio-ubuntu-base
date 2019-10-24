22e27bcd3753e68aac8b6611b33caa106ba63920
tringler <mail@thomas-ringler.de>
2019-10-24 17:27:56 +0200

    Update run.sh

--- ubuntu-base/run.sh
+++ ubuntu-base/run.sh
@@ -9,12 +9,12 @@ _term() {
 
 trap _term SIGTERM
 
+
 mkdir -p /var/run/homegear
 chown homegear:homegear /var/run/homegear
 /etc/homegear/homegear-start.sh
 homegear -c /config/homegear/config -u homegear -g homegear -p /var/run/homegear/homegear.pid &
 homegear-management -p /var/run/homegear/homegear-management.pid &
-homegear-webssh -p /var/run/homegear/homegear-webssh.pid &
-tail -f /var/log/homegear/homegear.log &
+tail -f /config/log/homegear/homegear.log &
 child=$!
 wait "$child"
